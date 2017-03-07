74dc4d180214a72d92ae28086cc47633db790cf9
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-03-07 23:56:55 +0900

    remove

--- Parcel/ParcelTests/ParcelTests.swift
+++ Parcel/ParcelTests/ParcelTests.swift
@@ -53,14 +53,6 @@ class ParcelTests: XCTestCase {
         }
     }
     
-    func _testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-            /*
-            self.expectation(description: "percel test")
-            self.waitForExpectations(timeout: 30)
- */
         }
     }
 
