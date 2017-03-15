16d3d316f5bafdb821cc1a732b9c3d6743e15e3d
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-03-16 02:40:54 +0900

    define methods

--- Sources/Server.swift
+++ Sources/Server.swift
@@ -7,30 +7,48 @@ public protocol ServerBehavior {
     associatedtype State
     associatedtype Message
     associatedtype Reply
+    associatedtype Error
     
     func initialize(config: Config) -> ServerInitResult<Self>
-    func onCall(state: State, request: Request, from: Parcel<Message>) -> ServerCallResult<Self>
-    func onCast(state: State, request: Request) -> ServerCastResult<Self>
-    func onReceive(state: State, message: Message) -> ServerCastResult<Self>
+    func onSendSync(state: State, request: Request, from: Parcel<Message>) -> ServerSendSyncResult<Self>
+    func onSendAsync(state: State, request: Request) -> ServerSendAsyncResult<Self>
+    func onReceive(state: State, message: Message) -> ServerReceiveResult<Self>
     func terminate(state: State, error: Error?)
     
 }
 
 public enum ServerInitResult<Behavior> where Behavior: ServerBehavior {
     case ok(state: Behavior.State, timeout: Int?)
-    case stop(Error)
+    case stop(Behavior.Error)
     case ignore
 }
 
-public enum ServerCallResult<Behavior> where Behavior: ServerBehavior {
+public enum ServerSendSyncResult<Behavior> where Behavior: ServerBehavior {
     case reply(state: Behavior.State, timeout: Int?, reply: Behavior.Reply?)
     case noreply(state: Behavior.State, timeout: Int?)
-    case stop(state: Behavior.State, error: Error, reply: Behavior.Reply?)
+    case stop(state: Behavior.State, error: Behavior.Error, reply: Behavior.Reply?)
 }
 
-public enum ServerCastResult<Behavior> where Behavior: ServerBehavior {
+public enum ServerSendAsyncResult<Behavior> where Behavior: ServerBehavior {
     case noreply(state: Behavior.State, timeout: Int?)
-    case stop(state: Behavior.State, error: Error)
+    case stop(state: Behavior.State, error: Behavior.Error)
+}
+
+public enum ServerReceiveResult<Behavior> where Behavior: ServerBehavior {
+    case noreply(state: Behavior.State, timeout: Int?)
+    case stop(state: Behavior.State, error: Behavior.Error)
+}
+
+public enum ServerRunResult<Behavior> where Behavior: ServerBehavior {
+    case ok(Parcel<Behavior.Message>)
+    case ignore
+    case error(Behavior.Error)
+}
+
+public struct ServerOption {
+    
+    public var timeout: Int?
+    
 }
 
 open class Server<Behavior> where Behavior: ServerBehavior {
@@ -41,8 +59,34 @@ open class Server<Behavior> where Behavior: ServerBehavior {
         self.behavior = behavior
     }
     
+    // MARK: Running Servers
+    
     public func run(config: Behavior.Config) {
         let _ = behavior.initialize(config: config)
+    }
+    
+    public func runAndLink(config: Behavior.Config, options: ServerOption) -> ServerRunResult<Behavior> {
+        return .ignore
+    }
+
+    public func stop(error: Error? = nil, timeout: Int? = nil) {
+        
+    }
+    
+    // MARK: Sending Requests
+    
+    public func sendSync(request: Behavior.Request, timeout: Int? = nil) -> Behavior.Reply? {
+        return nil
+    }
+    
+    public func sendAsync(request: Behavior.Request) {
+        
+    }
+    
+    // MARK: Sending Values to Clients
+    
+    public func sendReply(client: Parcel<Behavior.Message>, value: Behavior.Reply) {
+        
     }
     
 }
