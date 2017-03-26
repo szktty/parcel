619f850044fcb353007bbcb43a607c436021f728
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-03-26 21:13:32 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Parcel",
     dependencies: [
         .Package(url: "https://github.com/vapor/socks.git", majorVersion: 1),
-        .Package(url: "https://github.com/antitypical/Result", majorVersion: 3),
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
     ]
 )
