8b487a73018424dfec6a44f0d063496a05b66773
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-03-31 03:28:49 +0900

    terminate a parcel if onReceive handler is not found

--- Sources/Parcel.swift
+++ Sources/Parcel.swift
@@ -136,6 +136,8 @@ public final class Parcel<Message>: BasicParcel {
             case .break:
                 terminate()
             }
+        } else {
+            terminate()
         }
     }
     
