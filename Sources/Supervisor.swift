d21a3c830090cf19da9f9b3ff1bc083cd6255aa1
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-04-02 02:32:32 +0900

    add

--- Sources/Supervisor.swift
+++ Sources/Supervisor.swift
@@ -1,5 +1,57 @@
 import Foundation
+import Result
 
-public final class Supervisor {
+public protocol SupervisorDelegate {
+    
+    func setUp()
     
 }
+
+public enum SupervisorRestart {
+
+    case permanent
+    case transient
+    case temporary
+    
+}
+
+public enum SupervisorShutdown {
+    
+    case brutalKill
+    case timeout
+    
+}
+
+public enum SupervisorWorker {
+    
+    case worker
+    case supervisor
+    
+}
+
+public enum SupervisorStrategy {
+    
+    case oneForAll
+    case oneForOne
+    case restForOne
+    case simpleOneForOne
+    
+}
+
+public class SupervisorFlags {
+    
+}
+
+public class SupervisorChildSpecification<Message> {
+    
+    public var child: Parcel<Message>!
+    public var restart: SupervisorRestart!
+    
+}
+
+open class Supervisor {
+    
+    public var delegate: SupervisorDelegate?
+    
+}
+    
